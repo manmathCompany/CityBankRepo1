a677bbdaf98806c0370dd8a66445d2c3085113f6
TL <TL@gmail.com>
2020-04-12 00:08:02 +0530

    dev1 modified Test class

--- FirstApplication/src/com/mn/test/Test.java
+++ FirstApplication/src/com/mn/test/Test.java
@@ -1,8 +1,20 @@
 package com.mn.test;
 
+import com.mn.beans.Utility;
+
 public class Test {
 
 	public static void main(String[] args) {
 		
+		try {
+			
+			Utility u=new Utility();
+			
+		
+			System.out.println(u.sysDate());
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
 	}
 }
